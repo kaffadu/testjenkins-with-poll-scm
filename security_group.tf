839c5f2a070272ebe881c5eabcff2a1202163062
Patrick Affadu <kaffadu@Patricks-MacBook-Pro-3.local>
2023-10-16 01:14:13 +0100

    security_group update

--- security_group.tf
+++ security_group.tf
@@ -17,6 +17,13 @@ resource "aws_security_group" "seljenkins-sg" {
     cidr_blocks = ["0.0.0.0/0"] # Open http access 
     }
 
+    ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Open https access 
+    }
+
 
     egress {
     from_port   = 0
